646ad3e85d929e91dee6011c609fe9733fa4e19e
Kevin <khuai@ualberta.ca>
2024-10-24 11:55:42 -0600

    has city test failing

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -61,7 +61,9 @@ public class CustomList extends ArrayAdapter<City> {
     public void addCity(City city){
         cities.add(city);
     }
-
+    public boolean hasCity(City city) {
+        return cities.contains(city);
+    }
 
 
 }
